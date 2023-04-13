40b19aaa1970cc488b496fa182c8960556caa33a
William Douglas <williamdouglasgomes@hotmail.com>
2023-04-13 06:57:54 -0300

    Adicionado gets

--- FilmesApi/FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -10,17 +10,47 @@ namespace FilmesApi.Controllers
         public static List<Filme> filmes = new List<Filme>();
 
         [HttpPost]
-        public void AdicionaFilme([FromBody] Filme filme)
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
+            if(filme.Id == Guid.Empty)
+            {
+                filme.Id = Guid.NewGuid();
+            }
+
             filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
-            Console.WriteLine(filme.Duracao);
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { id = filme.Id }, filme);
         }
 
         [HttpGet]
-        public List<Filme> RecuperaFilmes()
+        [Route("RecuperaFilmes")]
+        public IEnumerable<Filme> RecuperaFilmes()
         {
             return filmes;
+        }
+
+        [HttpGet()]
+        [Route("RecuperaFilmesPaginados")]
+        public IEnumerable<Filme> RecuperaFilmesPaginados([FromQuery] int skip = 0, [FromQuery] int take = 10)
+        {
+            return filmes.Skip(skip).Take(take);
+        }
+
+        [HttpGet()]
+        [Route("RecuperaFilmesPorId")]
+        public IActionResult RecuperaFilmesPorId(Guid filmeId)
+        {
+            try
+            {
+                var filme = filmes.FirstOrDefault(f => f.Id == filmeId);
+
+                if (filme == null) return NotFound();
+
+                return Ok(filme);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- FilmesApi/FilmesApi/Models/Filme.cs
+++ FilmesApi/FilmesApi/Models/Filme.cs
@@ -4,11 +4,14 @@ namespace FilmesApi.Models
 {
     public class Filme
     {
-        [Required]
+        [Required(ErrorMessage = "O Id é um campo obrigatório")]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "O título do filme é obrigatório")]
         public string Titulo { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O gênero do filme é obrigatório")]
         public string Genero { get; set; }
-        [Required]
+        [Required(ErrorMessage = "A duração do filme é obrigatória")]
+        [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
         public int Duracao { get; set; }
     }
 }
