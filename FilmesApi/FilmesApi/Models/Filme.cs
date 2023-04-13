89410891219cc5f31d36c1800d5d3be265a2188b
William Douglas <williamdouglasgomes@hotmail.com>
2023-04-13 05:23:37 -0300

    Criando api de filmes

---
+++ FilmesApi/FilmesApi/Models/Filme.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
+{
+    public class Filme
+    {
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+        public string Genero { get; set; }
+        [Required]
+        public int Duracao { get; set; }
+    }
+}
