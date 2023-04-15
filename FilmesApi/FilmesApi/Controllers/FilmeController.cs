2b25c4e69a44fe2da985c314ea856f3e284fd208
William Douglas <williamdouglasgomes@hotmail.com>
2023-04-15 14:59:39 -0300

    Documentando a api

--- FilmesApi/FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -2,6 +2,7 @@
 using FilmesApi.Data;
 using FilmesApi.Data.Dtos;
 using FilmesApi.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesApi.Controllers
@@ -19,7 +20,15 @@ namespace FilmesApi.Controllers
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// Adiciona um filme ao banco de dados
+        /// </summary>
+        /// <param name="filmeDto"></param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso a inserção seja feita com sucesso</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [Route("AdicionaFilme")]
         public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
         {
             Filme filme = _mapper.Map<Filme>(filmeDto);
@@ -29,20 +38,26 @@ namespace FilmesApi.Controllers
             return CreatedAtAction(nameof(RecuperaFilmesPorId), new { id = filme.Id }, filme);
         }
 
-        [HttpGet]
-        [Route("RecuperaFilmes")]
-        public IEnumerable<Filme> RecuperaFilmes()
-        {
-            return _context.Filmes;
-        }
-
+        /// <summary>
+        /// Recupera um ou mais filmes do banco de dados, com opção de paginamento
+        /// </summary>
+        /// <param name="skip"></param>
+        /// <param name="take"></param>
+        /// <returns>IEnumerable</returns>
+        /// <response code="200">Caso a requisição seja feita com sucesso</response>
         [HttpGet()]
-        [Route("RecuperaFilmesPaginados")]
-        public IEnumerable<Filme> RecuperaFilmesPaginados([FromQuery] int skip = 0, [FromQuery] int take = 10)
+        [Route("RecuperaFilmes")]
+        public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 10)
         {
-            return _context.Filmes.Skip(skip).Take(take);
+            return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
         }
 
+        /// <summary>
+        /// Recupera um filme do banco de dados pelo id
+        /// </summary>
+        /// <param name="filmeId"></param>
+        /// <returns>IActionResult</returns>
+        /// <response code="200">Caso a requisição seja feita com sucesso</response>
         [HttpGet()]
         [Route("RecuperaFilmesPorId")]
         public IActionResult RecuperaFilmesPorId(Guid filmeId)
@@ -53,7 +68,89 @@ namespace FilmesApi.Controllers
 
                 if (filme == null) return NotFound();
 
-                return Ok(filme);
+                var filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+                return Ok(filmeDto);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// Atualiza um filme do banco de dados pelo id
+        /// </summary>
+        /// <param name="filmeId"></param>
+        /// <param name="filmeDto"></param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso a requisição seja feita com sucesso</response>
+        [HttpPut]
+        [Route("AtualizaFilme")]
+        public IActionResult AtualizaFilme(Guid filmeId, [FromBody] UpdateFilmeDto filmeDto)
+        {
+            try
+            {
+                var filme = _context.Filmes.FirstOrDefault(fil => fil.Id == filmeId);
+                if (filme == null) return NotFound();
+                _mapper.Map(filmeDto, filme);
+                _context.SaveChanges();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// Atualiza partes de um filme do banco de dados pelo id
+        /// </summary>
+        /// <param name="filmeId"></param>
+        /// <param name="patch"></param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso a requisição seja feita com sucesso</response>
+        [HttpPatch]
+        [Route("AtualizaFilmeParcial")]
+        public IActionResult AtualizaFilmeParcial(Guid filmeId, JsonPatchDocument<UpdateFilmeDto> patch)
+        {
+            try
+            {
+                var filme = _context.Filmes.FirstOrDefault(fil => fil.Id == filmeId);
+                if (filme == null) return NotFound();
+
+                var filmeParaAtualizar = _mapper.Map<UpdateFilmeDto>(filme);
+
+                patch.ApplyTo(filmeParaAtualizar, ModelState);
+                if (!TryValidateModel(filmeParaAtualizar)) return ValidationProblem(ModelState);
+
+                _mapper.Map(filmeParaAtualizar, filme);
+                _context.SaveChanges();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// Deleta um filme do banco de dados pelo id
+        /// </summary>
+        /// <param name="filmeId"></param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso a requisição seja feita com sucesso</response>
+        [HttpDelete]
+        [Route("Deletafilme")]
+        public IActionResult Deletafilme(Guid filmeId)
+        {
+            try
+            {
+                var filme = _context.Filmes.FirstOrDefault(fil => fil.Id == filmeId);
+                if (filme == null) return NotFound();
+
+                _context.Remove(filme);
+                _context.SaveChanges();
+                return NoContent();
             }
             catch(Exception ex)
             {
